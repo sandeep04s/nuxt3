e379f61893b8506a21f31aabce9249793fc04258
Sandeep Gupta <102574884+sandeep04s@users.noreply.github.com>
2025-03-20 15:23:10 +0530

    add counter

--- app.vue
+++ app.vue
@@ -2,5 +2,21 @@
   <div>
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
+    <h2>Hello</h2>
+    <button @click="increment">Increment</button>
+    <p>Count: {{ count }}</p>
   </div>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+
+const count = ref(0);
+
+const increment = () => {
+  count.value++;
+};            
+</script>
+
+<style scoped>
+</style>
