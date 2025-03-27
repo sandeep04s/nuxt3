e611013dee7186f9f87d43608ecbd0745a15ba57
Sandeep Gupta <102574884+sandeep04s@users.noreply.github.com>
2025-03-27 14:15:32 +0530

    update moviecard

---
+++ components/movie/Search.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import type { Movie, OMDBResponse } from "@/pages/movies/movies";
+const searchQuery=ref('page');
+const movies=ref<Movie[]>([]);
+const loading=ref<boolean>(false);
+const API_KEY = import.meta.env.VITE_OMDB_API_KEY;
+
+const handleSearchQuery= async()=>{
+ 
+  try {
+    loading.value=true;
+    const response: OMDBResponse = await $fetch(
+      `http://www.omdbapi.com/?apikey=${API_KEY}&s=${searchQuery.value}`
+    );
+
+    if (response.Response === "True") {
+      movies.value = response.Search;
+    } else {
+      console.error("Error fetching movies:", response.Error);
+      movies.value = [];
+    }
+  } catch (error) {
+    console.error("Network or API error:", error);
+    movies.value = [];
+  }
+  finally{
+    loading.value=false;
+  }
+
+}
+onMounted(()=>{
+    handleSearchQuery()
+})
+
+</script>
+
+<template>
+  <div>
+    Page: movies
+    <form @submit.prevent="handleSearchQuery">
+    <input type="search" v-model="searchQuery" :disabled="loading" placeholder="search movies">
+    <button >search</button>
+  </form>
+  <ul style="display: flex; flex-wrap: wrap; gap: 10px; list-style: none;">
+    <!-- <li v-for="(movie) in movies" :key="movie.imdbID">
+      <NuxtLink :to="{name:'movies-id',params:{id:movie.imdbID}}">
+        <img :src="movie.Poster" :alt="movie.Title"/>
+      </NuxtLink>
+    </li> -->
+    
+        <MovieCard :movie="movie" v-for="(movie) in movies" :key="movie.imdbID"/>
+      
+  </ul>
+  
+  </div>
+</template>
+
+<style scoped></style>
--- pages/movies/[id].vue
+++ pages/movies/[id].vue
@@ -1,14 +1,59 @@
 <script setup lang="ts">
-const { id } = useRoute().params;
+import MovieCard from '~/components/movie/MovieCard.vue';
+import type { movieData } from './movies';
+
+const id = useRoute().params.id as string;
+const API_KEY = import.meta.env.VITE_OMDB_API_KEY;
+
+const { data: movieDetails, status, error } =await useFetch<movieData>(`http://www.omdbapi.com/?apikey=${API_KEY}&i=${id}`, {
+  key: `/movies/${id}`,
+  pick: ['Title', 'imdbID', 'Poster'],
+  onResponse({ request, response }) {
+    if (response._data.Error) {
+      showError({ statusCode: 404, statusMessage: 'Page not found !!' })
+    }
+
+  }
+})
+
+
+// const { data: movieDetails, status, error, refresh, clear } = useAsyncData<movieData>(`/movies/${id}`, () => {
+//   return $fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&i=${id}`);
+// // }, {
+// //   transform(movieDetails) {
+// //     return {
+// //       Plot: movieDetails.Plot,
+// //       Title: movieDetails.Title,
+// //       Year: movieDetails.Year,
+// //       imdbID: movieDetails.Year,
+// //       Type: movieDetails.Type,
+// //       Poster: movieDetails.Poster
+// //     }
+// //   }
+// },{
+//    pick:['Title','imdbID','Poster'],
+//   // transform(data){
+//   //   return {
+//   //     Poster:data.Poster,
+//   //     imdbID:data.imdbID,
+//   //     Title:data.Title
+//   //   }
+//   // }
+// })
+
 </script>
 
 <template>
+
   <div>
-    Page: movies
-    <pre>{{ id }}</pre>
+    <h2>Movie Details</h2>
+
+    <div v-if="status === 'pending'">Loading...</div>
+    <div v-else-if="error">Error fetching movie data: {{ error.statusMessage }}</div>
+    <MovieCard v-else-if="movieDetails" :movie="movieDetails" />
   </div>
+
+
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
